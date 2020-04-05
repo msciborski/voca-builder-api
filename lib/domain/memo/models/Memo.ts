951c161465f046f748924d2f852e0a363dd714ec
msciborski <michal.sciborski@student.put.poznan.pl>
2020-04-05 17:36:20 +0200

    Use typegoose in memo/memogroup model

--- lib/domain/memo/models/Memo.ts
+++ lib/domain/memo/models/Memo.ts
@@ -1,23 +1,20 @@
+import { prop, getModelForClass } from "@typegoose/typegoose";
+
 export class Memo {
-    private id: string;
-    private sourceWord: string;
-    private translatedWord: string;
+    @prop()
+    _id: string;
+
+    @prop()
+    sourceWord: string;
+    
+    @prop()
+    translatedWord: string;
 
     constructor(id: string, sourceWord:string, translatedWord: string) {
-        this.id = id;
+        this._id = id;
         this.sourceWord = sourceWord;
         this.translatedWord = translatedWord;
     }
-
-    public getId() : string {
-        return this.id;
-    }
-
-    public getSourceWord() : string {
-        return this.sourceWord;
-    }
-
-    public getTranslatedWord(): string {
-        return this.translatedWord;
-    }
 }
+
+export const MemoModel = getModelForClass(Memo);
--- lib/domain/memo/models/MemoGroup.ts
+++ lib/domain/memo/models/MemoGroup.ts
@@ -1,32 +1,18 @@
 import { Memo } from "./Memo";
+import { prop, arrayProp, getModelForClass } from "@typegoose/typegoose";
 
 export class MemoGroup {
-    private id: string;
-    private name: string;
-    private memos: Memo[] = [];
+    @prop()
+    _id: string;
+
+    @prop()
+    name: string;
+
+    @arrayProp({ _id: true, items: Memo})
+    memos: Memo[] = [];
 
     constructor(id: string, name: string) {
-        this.id = id;
+        this._id = id;
         this.name = name;
-    }
-
-    public getId() : string {
-        return this.id;
-    }
-
-    public getName() : string {
-        return this.name;
-    }
-
-    public getMemos() : Memo[] {
-        return this.memos;
-    }
-
-    public addMemo(memo: Memo) : void {
-        this.memos.push(memo);
-    }
-
-    public removeMemo(memo: Memo) : void {
-        this.memos = this.memos.filter(m => m.getId() !== memo.getId());
-    }
+    }   
 }
